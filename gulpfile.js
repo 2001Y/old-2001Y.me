083c7ef3c62d5a5f07bd0747b4d6fe7b6fd030d5
2001Y <15167896+2001Y@users.noreply.github.com>
2019-03-07 01:14:20 +0900

    Bug Fix
    Signed-off-by: 2001Y <15167896+2001Y@users.noreply.github.com>

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,7 @@ gulp.task('autoprefixer', () =>
         .pipe(autoprefixer({
             stats: ['> 3% in JP']
         }))
+        .pipe(cssmin())
         .pipe(gulp.dest('themes/2001Y.me/static/style'))
 );
 
